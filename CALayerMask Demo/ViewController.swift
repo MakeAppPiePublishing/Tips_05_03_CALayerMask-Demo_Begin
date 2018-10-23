ad348632993705834dd196eb3477b2b292cd6888
Steve Lipton <steve@makeapppie.com>
2018-10-23 06:07:44 -0400

    Initial Commit

---
+++ CALayerMask Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CALayerMask Demo
+//
+//  Created by Steven Lipton on 10/23/18.
+//  Copyright © 2018 Steven Lipton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
