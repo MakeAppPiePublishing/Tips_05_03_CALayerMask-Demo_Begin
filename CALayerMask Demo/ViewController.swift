5abd05cbf2f912eda8c15a597abb995ca82d2de5
Steve Lipton <steve@makeapppie.com>
2018-11-02 06:35:19 -0500

    Initial Commit Begin State

--- CALayerMask Demo/ViewController.swift
+++ CALayerMask Demo/ViewController.swift
@@ -2,19 +2,46 @@
 //  ViewController.swift
 //  CALayerMask Demo
 //
-//  Created by Steven Lipton on 10/23/18.
-//  Copyright © 2018 Steven Lipton. All rights reserved.
+//  An exercise file for iOS Development Tips Weekly
+//  by Steven Lipton (C)2018, All rights reserved
+//  For videos go to http://bit.ly/TipsLinkedInLearning
+//  For code go to http://bit.ly/AppPieGithub
 //
 
 import UIKit
 
 class ViewController: UIViewController {
+    let upImage = UIImage(named:"thumb_up_48pt")
+    let downImage = UIImage(named: "thumb_down_48pt")
+    let inverseUpImage = UIImage(named: "invert_thumb_up_48pt")
+    let inverseDownImage = UIImage(named: "invert_thumb_down_48pt")
 
+   
+    
+    @IBOutlet weak var upButton: UIButton!
+    @IBOutlet weak var downButton: UIButton!
+    
+    @IBAction func upButton(_ sender: UIButton) {
+       
+    }
+    
+    @IBAction func downButton(_ sender: UIButton) {
+        
+    }
+    
+    func configureMask(button:UIButton, with image:UIImage!){
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
 
 }
 
